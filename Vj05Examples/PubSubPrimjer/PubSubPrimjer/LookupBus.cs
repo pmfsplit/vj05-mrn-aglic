ef236f8a8aab84c5b01daa96d4359d51e5f91f65
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-22 16:55:07 +0100

    Added pubsub example

---
+++ Vj05Examples/PubSubPrimjer/PubSubPrimjer/LookupBus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Event;
+
+namespace PubSubPrimjer
+{
+    public class MsgEnvelope
+    {
+        public string Topic { get; private set; }
+        public Object Payload { get; private set; }
+
+        public MsgEnvelope(string topic, object payload)
+        {
+            Topic = topic;
+            Payload = payload;
+        }
+    }
+
+    // 1. tip - tip eventa koji se publisha
+    // 2. tip - tip klasifikatora za klasifikaciju evenata
+    // 3. tip - tip subscribera koji mogu slušati na event
+    public class LookupBus : EventBus<MsgEnvelope, String, IActorRef> 
+    {
+        // je li jedna klasa podklasa sdruge
+        protected override bool IsSubClassification(string parent, string child)
+        {
+            return true;
+        }
+
+        // publisha događaj (event) subscriberu
+        protected override void Publish(MsgEnvelope @event, IActorRef subscriber)
+        {
+            subscriber.Tell(@event);
+        }
+
+        // 1. argument - događaj (event) koji se klasificira
+        // 2. argument - klasifikator koji se koristi da se klasificira događaj
+        protected override bool Classify(MsgEnvelope @event, string classifier)
+        {
+            return @event.Topic == classifier;
+        }
+        
+        // dobavlja vrijednost kojom klasificiramo pojedini događaj
+        protected override string GetClassifier(MsgEnvelope @event)
+        {
+            return @event.Topic;
+        }
+    }
+}
---
+++ Vj05Examples/PubSubPrimjer/PubSubPrimjer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Akka.Actor;
+
+namespace PubSubPrimjer
+{
+    public class PricedQuote
+    {
+    }
+
+    public class Price
+    {
+    }
+
+    public class QuoteListener : ReceiveActor
+    {
+        public QuoteListener()
+        {
+            Receive<PricedQuote>(x => Console.WriteLine($"{Self.Path} Dobio price quote"));
+        }
+    }
+
+    public class PriceListener : ReceiveActor
+    {
+        public PriceListener()
+        {
+            Receive<PricedQuote>(x => Console.WriteLine(x));
+            Receive<Price>(x => Console.WriteLine($"{Self.Path} Dobio price"));
+        }
+    }
+
+    public class TestActor : ReceiveActor
+    {
+        public TestActor()
+        {
+            Receive<MsgEnvelope>(x => Console.WriteLine($"{Self.Path}: {x.Payload}"));
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using (var system = ActorSystem.Create("pubsub"))
+            // {
+            //     var quoteListener = system.ActorOf(Props.Create(() => new QuoteListener()), "quotelistener");
+            //     var priceListener = system.ActorOf(Props.Create(() => new PriceListener()), "pricelistener");
+            //
+            //     system.EventStream.Subscribe(quoteListener, typeof(PricedQuote));
+            //     system.EventStream.Subscribe(priceListener, typeof(PricedQuote));
+            //     system.EventStream.Subscribe(priceListener, typeof(Price));
+            //
+            //     system.EventStream.Publish(new PricedQuote());
+            //     system.EventStream.Publish(new Price());
+            //     system.EventStream.Publish(new PricedQuote());
+            //
+            //     
+            //     // system.Terminate().Wait();
+            //     
+            //     system.WhenTerminated.Wait();
+            // }
+            using (var system = ActorSystem.Create("pubsub"))
+            {
+                var bus = new LookupBus();
+                var testActor = system.ActorOf(Props.Create(() => new TestActor()));
+                
+                // bus.Subscribe(testActor, "greetings");
+                bus.Subscribe(testActor, "sport");
+                bus.Publish(new MsgEnvelope("greetings", "hello"));
+                bus.Publish(new MsgEnvelope("sport", "Hajduk izgubio od Gorice"));
+                // bus.Publish("Hello world");
+                
+                system.WhenTerminated.Wait();
+            }
+        }
+    }
+}
